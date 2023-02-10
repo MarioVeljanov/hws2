8a00c53348d4c91dba595348c6b27a0add57ff3a
marioveljanov <marioveljanov1@gmail.com>
2023-02-10 16:36:17 +0100

    homework: hw06

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+ import React, { useEffect, useRef, useState } from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import { restoreState, saveState } from './localStorage/localStorage'
 import s2 from '../../s1-main/App.module.css'
@@ -18,8 +18,10 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        // делают студенты
-
+      // делают студенты
+    setValue(restoreState<string>("hw6-editable-span-value", 'hi'));
+      
+      
     }
 
     return (
