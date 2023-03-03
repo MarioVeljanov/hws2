2c9f02130e4c5842dbdba8d2945f6bc8857b958b
marioveljanov <marioveljanov1@gmail.com>
2023-03-03 15:56:00 +0100

    hw:09-2

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -13,9 +13,10 @@ function Clock() {
     const start = () => {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
+        
         const timer = setInterval(() => {
             setDate(new Date(restoreState('hw9-date', Date.now())))
-        })
+        }, 1000)
         
         setTimerId(Number(timer))
         
@@ -35,12 +36,12 @@ function Clock() {
 
     }
 
-    const stringTime = new Intl.DateTimeFormat("ru", { hour: 'numeric', minute: 'numeric', second: 'numeric' }).format(date); // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate =  new Intl.DateTimeFormat("ru").format(date); // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = new Intl.DateTimeFormat("ru", { hour: 'numeric', minute: 'numeric', second: 'numeric' }).format(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = new Intl.DateTimeFormat("ru").format(date) || <br />; ; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = new Intl.DateTimeFormat("en", { weekday: 'long' }).format(date); // пишут студенты
-    const stringMonth =  new Intl.DateTimeFormat("en", {month: "long"}).format(date);// пишут студенты
+    const stringDay = new Intl.DateTimeFormat("en", { weekday: 'long' }).format(date) || <br/> ; // пишут студенты
+    const stringMonth =  new Intl.DateTimeFormat("en", {month: "long"}).format(date) || <br/> ;// пишут студенты
 
     return (
         <div className={s.clock}>
