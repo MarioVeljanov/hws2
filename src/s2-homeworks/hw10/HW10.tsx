1661d67626c8edcd640db272a2243b4748c12b28
marioveljanov <marioveljanov1@gmail.com>
2023-03-11 15:41:32 +0100

    hw: 10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -14,12 +14,18 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
+    debugger
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
-
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading);
+    const dispatch = useDispatch();
+    // const isLoading = false
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
+        dispatch(loadingAC(true));
 
+        setTimeout(() => {
+            dispatch(loadingAC(false));
+        }, 1500)
         // setTimeout
     }
 
